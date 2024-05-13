77c82a976acfb053492b8264a56c69720489121a
simoncoolsaet <simoncoolsaet@gmail.com>
2024-05-13 21:55:43 +0100

    Fixed the GUI!

--- Code/Library/warwick_pmsc_skylab/__init__.py
+++ Code/Library/warwick_pmsc_skylab/__init__.py
@@ -1,4 +1,8 @@
 #Universal Constants
+from warwick_pmsc_skylab.GUI_2 import MainWindow
+import sys
+from PyQt5.QtWidgets import QApplication
+
 radius_polar = 6356752
 radius_equatorial = 6378137
 earth_eccentricity_squared = 6.694379e-3
@@ -8,3 +12,10 @@ rho_0 = 1.225
 H = 8400.0
 stellar_day  = 86164.1
 earth_rotation_s = 1/360 * stellar_day
+
+def run_GUI(window_class=MainWindow):
+    #app = 0
+    app = QApplication(sys.argv)
+    w = window_class()
+    w.show()
+    sys.exit(app.exec_())
