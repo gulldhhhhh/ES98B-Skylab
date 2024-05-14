081664c2ddf5e6dc0a880af2c33c43d2b4005e90
simoncoolsaet <simoncoolsaet@gmail.com>
2024-05-15 00:17:10 +0100

    Trying to add dependencies

---
+++ Code/Library/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name = 'warwick_pmsc_skylab',
+    version = '0.1.1',
+    authors = ['Simon Coolsaet', 'Kyle Berwick', 'Gullveig Liang', 'Tanya Sanjeev', 'Leixin Xu'],
+    author_email = ['Simon.Coolsaet@warwick.ac.uk', 'Kyle.Berwick@warwick.ac.uk', 'Gullveig.Liang@warwick.ac.uk', 'Tanya.Sanjeev@warwick.ac.uk', 'Leixin.Xu@warwick.ac.uk'],
+    description = "Warwick PMSC Group Project (Skylab Team)",
+    classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+    python_requires='>=3.7',
+    packages=['warwick_pmsc_skylab'],
+    install_requires = ['numpy'])
