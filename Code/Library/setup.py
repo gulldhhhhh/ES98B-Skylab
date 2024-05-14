9909cfb6276c4e05fd2a7c56acadf44acb108a6f
simoncoolsaet <simoncoolsaet@gmail.com>
2024-05-15 00:26:52 +0100

    Figured out stuff to do with dependencies

--- Code/Library/setup.py
+++ Code/Library/setup.py
@@ -6,11 +6,7 @@ setup(
     authors = ['Simon Coolsaet', 'Kyle Berwick', 'Gullveig Liang', 'Tanya Sanjeev', 'Leixin Xu'],
     author_email = ['Simon.Coolsaet@warwick.ac.uk', 'Kyle.Berwick@warwick.ac.uk', 'Gullveig.Liang@warwick.ac.uk', 'Tanya.Sanjeev@warwick.ac.uk', 'Leixin.Xu@warwick.ac.uk'],
     description = "Warwick PMSC Group Project (Skylab Team)",
-    classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
-        ],
+    classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License", "Operating System :: OS Independent",],
     python_requires='>=3.7',
     packages=['warwick_pmsc_skylab'],
     install_requires = ['numpy'])
